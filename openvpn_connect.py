326b56c154ba9c35e059f44d29a127bd008fc9b3
root <root@phobos.zakx.de>
2011-06-05 20:35:53 +0200

    fixing a rare issue where there's more than one waiting Connection object

--- openvpn_connect.py
+++ openvpn_connect.py
@@ -12,37 +12,39 @@ import os
 import sys
 import datetime
 
+
+def report(error):
+        f = open("/tmp/log", "a")
+        f.write("%s\n" % error)
+	print error
+        f.close()
 env_certuser = os.environ.get("common_name")
 env_user = os.environ.get("username")
 if not env_user:
 	env_user = env_certuser
 env_remote_ip = os.environ.get("trusted_ip")
 env_vpn_ip = os.environ.get("ifconfig_pool_remote_ip")
-
 try:
 	user = User.objects.get(username=env_user)
 except User.DoesNotExist:
-	print "user does not exist"
+	report("user does not exist: %s" % env_user)
 	sys.exit(1)
-
 try:
-	c = Connection.objects.get(user=user, remote_ip=env_remote_ip, vpn_ip=None)
+	c = Connection.objects.filter(user=user, remote_ip=env_remote_ip, vpn_ip=None)[0]
 except Connection.DoesNotExist:
-	print "missing a waiting connection"
+	report("missing a waiting connection: %s" % user)
 	sys.exit(1)
-
 c.vpn_ip = env_vpn_ip
 try:
 	c.save()
 except:
-	print "couldn't save connection"
+	report("couldn't save connection: %s" % user)
 	c.delete()
 	sys.exit(1)
-
 try:
 	tn = TrafficNode(user=user, connection=c, time=datetime.datetime.now())
 	tn.save()
 except Exception, e:
-	print "tn failed: %s" % e
+	report("tn failed: %s (for %s)" % (e, user))
 
-print "user may connect"
+report("user may connect: %s" % user)
