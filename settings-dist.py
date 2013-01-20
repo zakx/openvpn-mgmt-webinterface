3a67b66134f0288b0b1087f98f474fb2bbdb1aa4
zakx <sg@unkreativ.org>
2013-01-20 02:10:29 +0100

    Cleaned up settings file

--- settings-dist.py
+++ settings-dist.py
@@ -1,99 +1,77 @@
-# Django settings for purple project.
+# Django settings.
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+DEBUG = False
 
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
-
-MANAGERS = ADMINS
-
+BASE_PATH = '/opt/purple/' # set this to your base path
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'purple.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',        # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'NAME': 'purple.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
     }
 }
+SECRET_KEY = 'SET THIS TO A RANDOM STRING'
+ADMINS = (
+    ('Your Name', 'mail@example.com'),
+)
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'Europe/Berlin'
+# password for the openvpn management interface
+OPENVPN_PASSWORD = "change me"
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+# delete the following three lines and you're done configuring
+import sys
+print "Please configure c4sh in settings.py. Your hostname is %s." % socket.gethostname()
+sys.exit(0)
 
-SITE_ID = 1
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = "purple.sendmail.EmailBackend"
+    SESSION_COOKIE_SECURE = True
+    SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
-USE_L10N = True
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/opt/purple/media/'
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
+TEMPLATE_DEBUG = DEBUG
+APP_URL = "app"
+MEDIA_ROOT = BASE_PATH + 'media/'
 MEDIA_URL = '/media/'
+STATIC_ROOT = BASE_PATH + 'static/'
+STATIC_URL = '/static/'
+TEMPLATE_DIRS = (
+    BASE_PATH + "templates",
+)
+MANAGERS = ADMINS
+TIME_ZONE = 'Europe/Berlin'
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+AUTH_PROFILE_MODULE = 'app.UserProfile'
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/amedia/'
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
-# Make this unique, and don't share it with anybody.
-print "change SECRET_KEY in settings.py!"
-SECRET_KEY = '5^6f%@@2+(v+(8^@fw!0kf(%n6%!ym^h$20d37i$jwFIXME'
-
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
 )
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-	"django.core.context_processors.auth",
-	"django.core.context_processors.debug",
-	"django.core.context_processors.i18n",
-	'django.core.context_processors.media',
-	'django.contrib.messages.context_processors.messages',
-)
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.transaction.TransactionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
+
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
-
-
 ROOT_URLCONF = 'purple.urls'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    "/opt/purple/templates",
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -101,14 +79,30 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
+    'django.contrib.staticfiles',
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
     'purple.app',
+    'south',
+    'django_extensions',
 )
-AUTH_PROFILE_MODULE = 'app.UserProfile'
-EMAIL_BACKEND = "purple.sendmail.EmailBackend"
 
-# password for the openvpn management interface
-OPENVPN_PASSWORD = "welle:erdball"
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
